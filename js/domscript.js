a859fe0c163aee4d2ce504637499587db7cdab42
Bernhard J. Vogler <mail@bernhardvogler.de>
2016-05-16 10:07:12 +0200

    Enable retina support for map

--- js/domscript.js
+++ js/domscript.js
@@ -5,6 +5,7 @@ $(function(){
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
+    detectRetina: true,
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiYmVybmhhcmR2b2dsZXIiLCJhIjoiY2lsZjFrMXE2MDAyeHc5bTQxa2Rob293ZCJ9.rzwxlAgOdr-6YhTwYfF-jA'
 }).addTo(map);
