f48b9069d04223475decc9590147869988765f65
ynux <ynux@gmx.net>
2016-08-29 00:19:07 +0200

    comment and delete bad )

--- js/domscript.js
+++ js/domscript.js
@@ -18,14 +18,14 @@ $(function(){
 
 	  		if (data[i].long != '' && data[i].lat != '') {
 
+                                // Portale haben Radius 2000  ... Ich weiss, implicit is bad
 	  			if (parseInt(data[i].radius) == 2000) {
 	  				var marker = L.circle([data[i].lat, data[i].long], data[i].radius, {
 	  				    color: '#ff9933',
 	  				    fillColor: '#D9EB16',
 	  				    fillOpacity: 0.4
 	  				}).addTo(map);
-                                } else if {
-	  			  (parseInt(data[i].radius) > 0) {
+                                } else if (parseInt(data[i].radius) > 0) {
 	  				var marker = L.circle([data[i].lat, data[i].long], data[i].radius, {
 	  				    color: '#ff9933',
 	  				    fillColor: '#D9EB76',
